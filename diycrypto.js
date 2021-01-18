a1f64e69ffbf9f7881715b45e4bd2fc666ffa920
Kim Örnberg <kim.ornberg@skola.botkyrka.se>
2021-01-18 16:02:13 +0100

    Import/Export

---
+++ diycrypto.js
@@ -0,0 +1,26 @@
+export function ceasarEncypt(msg, shift)
+
+{
+    const charCodes = []
+        for (let i = 0; i < msg.length; i++) {
+            charCodes.push(msg[i].charCodeAt(0))
+        }
+
+
+        for (let i = 0; i < charCodes.length; i++) {
+    charCodes[i] = charCodes[i] + shift
+            
+        }
+
+
+    let result = ""
+    for (let i = 0; i < charCodes.length; i++) {
+    result = result + String.fromCharCode(charCodes[i])
+    }
+
+return result
+}
+
+export function  ceasarDecypt(encryptedMsg, shift) {
+
+}
--- main.js
+++ main.js
@@ -1,29 +1,3 @@
+import { ceasarEncypt, ceasarDecypt } from "./diycrypto.js"
 
-function ceasarEncypt(msg)
-
-{
-    const charCodes = []
-        for (let i = 0; i < msg.length; i++) {
-            charCodes.push(msg[i].charCodeAt(0))
-        }
-
-
-        for (let i = 0; i < charCodes.length; i++) {
-    charCodes[i] = charCodes[i] + 1
-            
-        }
-
-
-    let result = ""
-    for (let i = 0; i < charCodes.length; i++) {
-    result = result + String.fromCharCode(charCodes[i])
-    }
-
-return result
-}
-
-const secretMessage = "RETREATTOMORROW"
-const encryptedMessage = ceasarEncypt(secretMessage)
-
-
-console.log(secretMessage, encryptedMessage)
+console.log(ceasarEncypt("Hello", 1))
